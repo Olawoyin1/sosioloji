2411c3ff6223bec30ea2d84547d63ee6807b76c7
Olawoyin <yustee2017@gmail.com>
2025-07-08 22:58:58 +0100

    movement page done

--- src/Sections/Contributors.tsx
+++ src/Sections/Contributors.tsx
@@ -43,9 +43,9 @@ const Contributors = () => {
 ];
 
   return (
-    <section className="b">
+    <section className="">
 
-    <div className=" py-10 h-screen  flex items-start justify-center flex-col px-8 md:px-12">
+    <div className=" py-10 min-h-screen  flex items-start justify-center flex-col px-8 md:px-12">
       <h1 className="text-2xl md:text-3xl font-extrabold mb-10">Contributing Authors</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-9 md:gap-20 place-items-center">
         {contributors.map((person, index) => (
--- src/Sections/Movement.tsx
+++ src/Sections/Movement.tsx
@@ -2,81 +2,88 @@ import React from "react";
 import FancyLinkButton from "../components/Button";
 import { Link } from "react-router-dom";
 
-
-
 const Movement: React.FC = () => {
   return (
-    <div className="relative ml-9 md:ml-0   ">
-
-        <Link to="/">
-                      <span className="block z-50 md:hidden">sosioloji</span>
-                    </Link>
-
-      {/* Rotated Header */}
-      <h3 className="absolute left-[-84px] sm:left-[-100px] top-1/2 transform rotate-270 text-sm md:text-xl font-bold text-center writing-vertical text-[#1E1B1B] font-playfair">
-        Get Insight
-
-
-        <span className="text-blue-500 inline-block ml-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 576 512"
-            width="14"
-            height="14"
-            fill="currentColor"
-          >
-            <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
-          </svg>
-        </span>
-      </h3>
-
-      {/* Main Tag Container */}
-      <div
-        className="relative w-full bg-[#FFF0D3] flex items-center justify-center rounded-[15px] shadow-md p-4 md:h-[400px] h-auto"
-        id="tag-box"
+    <section className="min-h-screen flex  items-center flex-col justify-center">
+      <Link
+        to="/"
+        className="text-3xl md:text-6xl font-bold tracking-tight text-gray-800 fixed bottom-17"
       >
+        <span className="block z-50">sosioloji</span>
+      </Link>
 
+      <div className="max-w-7xl mx-auto px-8 md:px-12">
+        <div className="relative ml-9 md:ml-0   ">
+          {/* Rotated Header */}
+          <h3 className="absolute left-[-84px] sm:left-[-100px] top-1/2 transform rotate-270 text-sm md:text-xl font-bold text-center writing-vertical text-[#1E1B1B] font-playfair">
+            Get Insight
+            <span className="text-blue-500 inline-block ml-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 576 512"
+                width="14"
+                height="14"
+                fill="currentColor"
+              >
+                <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
+              </svg>
+            </span>
+          </h3>
 
-        <div className="flex flex-wrap space-x-3 items-center w-full">
-          
+          {/* Main Tag Container */}
+          <div
+            className="relative w-full bg-[#FFF0D3] flex items-center justify-center rounded-[15px] shadow-md p-4 sm:h-[400px] h-auto"
+            id="tag-box"
+          >
+            <div className="flex flex-wrap space-x-3 items-center w-full">
+              <form
+                className="subscribe-form mt-4"
+                data-members-form="subscribe"
+              >
+                <h5 className="text-2xl md:text-4xl text-center mb-4 md:mb-7 font-bold">
+                  Join The Movement
+                </h5>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="name"
+                    className="w-full px-7 py-3 pf text-gray-700 bg-white border border-gray-300 rounded-3xl text-sm outline-none focus:ring-0 focus:ring-pink-300 transition"
+                    required
+                    placeholder="Enter your name"
+                    // value={formData.name}
+                    // onChange={handleChange}
+                  />
+                  <input
+                    type="email"
+                    name="email"
+                    className="w-full mt-2 sm:mt-4 sm:mb-3 px-7 py-3 pf text-gray-700 bg-white border border-gray-300 rounded-3xl text-sm outline-none focus:ring-0 focus:ring-pink-300 transition"
+                    required
+                    placeholder="Enter your email"
+                    // value={formData.email}
+                    // onChange={handleChange}
+                  />
 
-          <form className="subscribe-form mt-4" data-members-form="subscribe">
-            <h5 className="text-2xl md:text-4xl text-center mb-4 md:mb-7 font-bold">Join The Movement</h5>
-            <div className="form-group">
-              <input
-                type="text"
-                name="name" 
-                className="w-full px-7 py-3 pf text-gray-700 bg-white border border-gray-300 rounded-3xl text-sm outline-none focus:ring-0 focus:ring-pink-300 transition"
-                required
-                placeholder="Enter your name"
-                // value={formData.name}
-                // onChange={handleChange}
-              />
-              <input
-                type="email"
-                name="email"className="w-full mt-4 mb-3 px-7 py-3 pf text-gray-700 bg-white border border-gray-300 rounded-3xl text-sm outline-none focus:ring-0 focus:ring-pink-300 transition"
-                required
-                placeholder="Enter your email"
-                // value={formData.email}
-                // onChange={handleChange}
-              />
-
-              <FancyLinkButton label="Submit" textColor="black" bgColor='#FEBDD0'  block to="/"  />
+                  <FancyLinkButton
+                    label="Submit"
+                    textColor="black"
+                    bgColor="#FEBDD0"
+                    block
+                    to="/"
+                  />
+                </div>
+              </form>
             </div>
-          </form>
+          </div>
 
-
-
-        </div>
-      </div>
-
-      {/* Decorative Span below bottom-right, shifted left */}
-      <span
-        className="absolute border border-gray-700 rounded-[15px] opacity-50 z-[-1] 
+          {/* Decorative Span below bottom-right, shifted left */}
+          <span
+            className="absolute border border-gray-700 rounded-[15px] opacity-50 z-[-1] 
     bottom-[-14px] right-4  
     w-full h-full"
-      />
-    </div>
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
