766dbcf2582a0f4ea714806cbaec5dda744ba2a4
Olawoyin <yustee2017@gmail.com>
2025-07-08 19:35:07 +0100

    some image fix on contibutors

--- src/Sections/Contributors.tsx
+++ src/Sections/Contributors.tsx
@@ -6,7 +6,7 @@ const Contributors = () => {
   const contributors = [
   {
     name: "Olawoyin Gbolahan",
-    imageUrl: "https://i.pravatar.cc/300?img=5",
+    imageUrl: "https://res.cloudinary.com/dunid4t4g/image/upload/v1751881220/ghcr8a6eesmyacnqnrkn.webp",
     subTags: ["Sosioloji", "Contributor"],
     socials: [
       { icon: <FaXTwitter size={22} className="text-blu-500" />, onClick: () => console.log("Twitter - Gbolahan") },
@@ -15,7 +15,7 @@ const Contributors = () => {
   },
   {
     name: "Jonathan Doe",
-    imageUrl: "https://i.pravatar.cc/300?img=12",
+    imageUrl: "https://res.cloudinary.com/dunid4t4g/image/upload/v1751875303/uastl3o4bafvs7izkuwy.webp",
     subTags: ["Editor"],
     socials: [
       { icon: <FaXTwitter size={22} className="text-bue-500" />, onClick: () => console.log("Twitter - Gbolahan") },
@@ -24,7 +24,7 @@ const Contributors = () => {
   },
   {
     name: "Sosioloji",
-    imageUrl: "https://i.pravatar.cc/300?img=18",
+    imageUrl: "https://res.cloudinary.com/dunid4t4g/image/upload/v1751583678/ilaqalb7z6ngg2a6diwh.webp",
     subTags: ["Movement", "Activist"],
     socials: [
       { icon: <FaXTwitter size={22} className="text-lue-500" />, onClick: () => console.log("Twitter - Gbolahan") },
@@ -33,7 +33,7 @@ const Contributors = () => {
   },
   {
     name: "WiseAndSane",
-    imageUrl: "https://i.pravatar.cc/300?img=8",
+    imageUrl: "https://res.cloudinary.com/dunid4t4g/image/upload/v1751583500/n9o8a0h4wpca7mjtqmjk.webp",
     subTags: ["Wise & Sane", "Member"],
     socials: [
       { icon: <FaXTwitter size={22} className="text-bue-500" />, onClick: () => console.log("Twitter - Gbolahan") },
@@ -43,7 +43,7 @@ const Contributors = () => {
 ];
 
   return (
-    <div className=" py-10 px-8 md:px-12">
+    <div className=" py-10 h-screen flex items-start justify-center flex-col px-8 md:px-12">
       <h1 className="text-2xl md:text-3xl font-extrabold mb-10">Contributing Authors</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-9 md:gap-20 place-items-center">
         {contributors.map((person, index) => (
